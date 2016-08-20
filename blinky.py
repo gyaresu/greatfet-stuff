c3f501f8e5d9c806101edd106d6bbab22791bb37
Gareth <me@gareth.codes>
2016-08-20 14:23:32 -0700

    blinky

---
+++ blinky.py
@@ -0,0 +1,18 @@
+# This is some blinky code to show Python interaction with Great Scott Gadget's new GreatFET "Azalea"
+# http://greatscottgadgets.com/greatfet/
+#
+# Run the below code from the 'host' directory to imprt greatfet as a module
+# https://github.com/dominicgs/GreatFET-experimental/tree/master/host
+
+from greatfet import GreatFET, protocol
+from time import sleep
+from random import randint
+
+board = GreatFET()
+
+def blinky(times, delay):
+    for i in range(0, times):
+        board.vendor_request_out(protocol.vendor_requests.LED_TOGGLE,randint(2,4))
+        sleep(delay)
+
+blinky(100, 0.1)
